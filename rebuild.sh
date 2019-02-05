d9708292db497899589b61ad91fd8e4b4836d7ef
anon <christopher.kullenberg@gmail.com>
2019-02-05 11:52:09 +0100

    added main menu selector bar

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,8 @@
+
+# To purge old container images:
+# Use bash (wont work in fish):
+# sudo docker rmi $(sudo docker images -f "dangling=true" -q)
+
 sudo docker stop mycontainer
 sudo docker rm mycontainer
 sudo docker build -t myimage .
