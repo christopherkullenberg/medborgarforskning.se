042abaa091107325b7677f1fcea4e4e805707714
anon <christopher.kullenberg@gmail.com>
2019-02-19 11:26:01 +0100

    started building the webapp that queries projects

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.7
 
-ENV STATIC_INDEX 1
+# ENV STATIC_INDEX 1
 
 COPY ./app /app
 
--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
-from bs4 import BeautifulSoup
-import requests
-import lxml
+#from bs4 import BeautifulSoup
+#import requests
+#import lxml
 app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template("index.html", query="root")
+
+@app.route("/<string:query>/")
+def query(query):
+    return render_template('index.html', query=query)
 
 @app.route("/hello")
 def hello():
     return "Hello World from Flaskan. Testar att starta om"
 
-@app.route("/hello/<string:name>/")
-def helloName(name):
-    return render_template('namn.html', name=name)
 
 @app.route("/gendercounter/<string:name>/")
 def getGender(name):
@@ -18,17 +23,22 @@ def getGender(name):
     namnet = gendercounter.from_string(name)
     return str(namnet.genderfrequency())
 
-@app.route("/")
-def main():
-    index_path = os.path.join(app.static_folder, 'index.html')
-    return send_file(index_path)
 
-@app.route("/flashbackscraper")
-def fbscraper():
-    r = requests.get("https://telecomix.org/")
-    html = r.content
+
+
+
+#@app.route("/")
+#def main():
+#    return render_template('index.html', query="index")
+    #index_path = os.path.join(app.static_folder, 'index.html')
+    #return send_file(index_path)
+
+#@app.route("/flashbackscraper")
+#def fbscraper():
+#    r = requests.get("https://telecomix.org/")
+#    html = r.content
     #soup = BeautifulSoup(html, "lxml")
-    return html
+#    return html
 
 
 
