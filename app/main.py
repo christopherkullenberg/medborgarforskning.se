204415a999d598040bd9c726a1c12b6adfe4cf92
anon <christopher.kullenberg@gmail.com>
2019-02-21 15:05:29 +0100

    added sqlite3 db for projects and integration with selector

--- app/main.py
+++ app/main.py
@@ -1,42 +1,47 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+
+import sqlite3
+# CREATE TABLE projects(id INTEGER PRIMARY KEY, name TEXT, datecreated TEXT, numberofparticipants INT, keywords TEXT);
+
 app = Flask(__name__)
 
-
-
-
-temp = '''         <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        '''
-
-
-
-
-
+db = sqlite3.connect('testdb.sqlite3')
+cursor = db.cursor()
+dbquery = cursor.execute('SELECT * FROM projects;') 
 
 @app.route("/")
 def root():
-    result = {'Projekt 1':"Naturvetenskap",
-              'Projekt 2':"humaniora",
-              'Projekt 3':"skola"}
-    return render_template("index.html", result = result)
+    selected = {'typeofproject': 'all forskning'}
+    return render_template("index.html", 
+                           result = selected, 
+                           selection = selected['typeofproject'])
 
-@app.route("/<string:query>/")
+@app.route("/<string:query>/") # use this for search function
 def query(query):
     result = {'Projektnamn': query}
     return render_template('index.html', result = result)
+
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        selected = request.form
+        db = sqlite3.connect('testdb.sqlite3')
+        cursor = db.cursor()
+        dbquery = cursor.execute('SELECT * FROM projects WHERE keywords == "' +
+                                 selected["typeofproject"] + '";') 
+        selectionresults = {}
+        for d in dbquery:
+            selectionresults[d[1]] = d[4]
+        
+        print(selected['typeofproject'])
+        return render_template("index.html", 
+                               result = selectionresults, 
+                               selection = selected['typeofproject']
+                               )
+    
+
+
 
 @app.route("/hello")
 def hello():
