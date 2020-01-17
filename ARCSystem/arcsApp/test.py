b4c0c3e546358e4a7d39863c15b30bbc3fb11264
anon <anon@pop-os.localdomain>
2020-01-17 11:27:40 +0100

    now working test server

--- ARCSystem/arcsApp/test.py
+++
@@ -1,7 +0,0 @@
-#
-# File Name: test.py
-# Description: test if the uwsgi configuration to serve applications is functioning
-#
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"] # python3
