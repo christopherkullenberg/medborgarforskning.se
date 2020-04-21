7c7a15bb22b3d357f639ca7e989f31fbfa90093e
christopherkullenberg <christopher.kullenberg@gmail.com>
2020-04-21 15:19:41 +0200

    code cleanup

--- ArcsSystem/blog/models.py
+++ ArcsSystem/blog/models.py
@@ -1,16 +1,11 @@
 from django.db import models
 from taggit.managers import TaggableManager
 
-'''
-class Blog(models.Model):
-    name = models.CharField(max_length=100)
-    tagline = models.TextField()
-
-    def __str__(self):
-        return self.name
-'''
 
 class Post(models.Model):
+    '''
+    This is the basic model for a blog post
+    '''
     title = models.CharField(max_length=100, default='title')
     published = models.DateField()
     content = models.TextField()
@@ -26,20 +21,3 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
-
-'''
-class Entry(models.Model):
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    headline = models.CharField(max_length=255)
-    body_text = models.TextField()
-    pub_date = models.DateField()
-    mod_date = models.DateField()
-    authors = models.ManyToManyField(Author)
-    number_of_comments = models.IntegerField()
-    number_of_pingbacks = models.IntegerField()
-    rating = models.IntegerField()
-
-    def __str__(self):
-        return self.headline
-'''
-# Create your models here.
