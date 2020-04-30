d939016501726a95c3639c77c96148db86f630fb
Jonathan Brier <gitcommit@brierjon.com>
2020-04-30 07:57:53 +0200

    merge settings comments from AllAuth to settings.py

--- ArcsSystem/arcs_project/settings.py
+++ ArcsSystem/arcs_project/settings.py
@@ -195,33 +195,89 @@ USE_TZ = True
 
 ### AllAuth Config ###
 # Configuration options: https://django-allauth.readthedocs.io/en/latest/configuration.html
+# Setting comments are a combination of the AllAuth settings example and added clarifying text.
 
 #Account adapters
 #ACCOUNT_ADAPTER = 'arcsauthenticator.adapter.CustomProcessAdapter' # specifies the adapter to modify default behavior else DefaultAccountAdapter
 
 #all-auth registraion settings
+
+# Specifies the login method to use -- whether the user logs in by entering
+# their username, e-mail address, or either one of both. Possible values
+# are 'username' | 'email' | 'username_email'
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+
+# The URL to redirect to after a successful e-mail confirmation, in case no
+# user is logged in. Default value is settings.LOGIN_URL.
+# ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL
+
+# The URL to redirect to after a successful e-mail confirmation, in case of
+# an authenticated user. Default is settings.LOGIN_REDIRECT_URL
+# ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL
+
+# Determines the expiration date of email confirmation mails (# of days).
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
-ACCOUNT_EMAIL_CONFIRMATION_HMAC = True
+
+# The user is required to hand over an e-mail address when signing up.
 ACCOUNT_EMAIL_REQUIRED = True
+
+# Determines the e-mail verification method during signup. When set to
+# "mandatory" the user is blocked from logging in until the email
+# address is verified. Choose "optional" or "none" to allow logins
+# with an unverified e-mail address. In case of "optional", the e-mail
+# verification mail is still sent, whereas in case of "none" no e-mail
+# verification mails are sent.
 ACCOUNT_EMAIL_VERIFICATION = "optional"
-ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
-ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
-ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 86400 # 1 day. This does ot prevent admin login frombeing brut forced.
-ACCOUNT_LOGOUT_REDIRECT_URL ='/accounts/login/' #or any other page
-LOGIN_REDIRECT_URL = '/accounts/email/' # redirects to profile page by default
-ACCOUNT_PRESERVE_USERNAME_CASING = False # reduces the delays in iexact lookups
-ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
-ACCOUNT_UNIQUE_EMAIL=True
-ACCOUNT_USERNAME_MIN_LENGTH = 5
-ACCOUNT_USERNAME_REQUIRED =True
-ACCOUNT_USERNAME_VALIDATORS = None
-ACCOUNT_AUTHENTICATION_METHOD= "username_email" #users can login with either their userename or email
 
+# Subject-line prefix to use for email messages sent. By default, the name
+# of the current Site (django.contrib.sites) is used.
+# ACCOUNT_EMAIL_SUBJECT_PREFIX = '[Site] '
 
-#Account Signup
-ACCOUNT_FORMS = {'signup': '.forms.SignupForm',}
+# A string pointing to a custom form class
+# (e.g. 'myapp.forms.SignupForm') that is used during signup to ask
+# the user for additional input (e.g. newsletter signup, birth
+# date). This class should implement a `def signup(self, request, user)`
+# method, where user represents the newly signed up user.
+# ACCOUNT_SIGNUP_FORM_CLASS = None
 
-#Social Account Settings - Provider specific settings
+# When signing up, let the user type in their password twice to avoid typ-o's.
+ACCOUNT_SIGNUP_PASSWORD_VERIFICATION = True
+
+# Enforce uniqueness of e-mail addresses.
+ACCOUNT_UNIQUE_EMAIL = True
+
+# A callable (or string of the form 'some.module.callable_name') that takes
+# a user as its only argument and returns the display name of the user. The
+# default implementation returns user.username.
+# ACCOUNT_USER_DISPLAY
+
+# An integer specifying the minimum allowed length of a username.
+ACCOUNT_USERNAME_MIN_LENGTH = 8
+
+# The user is required to enter a username when signing up. Note that the
+# user will be asked to do so even if ACCOUNT_AUTHENTICATION_METHOD is set
+# to email. Set to False when you do not wish to prompt the user to enter a
+# username.
+ACCOUNT_USERNAME_REQUIRED = True
+
+# render_value parameter as passed to PasswordInput fields.
+# ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = False
+
+# Request e-mail address from 3rd party account provider? E.g. using OpenID
+# AX, or the Facebook 'email' permission.
+# SOCIALACCOUNT_QUERY_EMAIL = ACCOUNT_EMAIL_REQUIRED
+
+# Attempt to bypass the signup form by using fields (e.g. username, email)
+# retrieved from the social account provider. If a conflict arises due to a
+# duplicate e-mail address the signup form will still kick in.
+# SOCIALACCOUNT_AUTO_SIGNUP = True
+
+# Enable support for django-avatar. When enabled, the profile image of the
+# user is copied locally into django-avatar at signup. Default is
+# 'avatar' in settings.INSTALLED_APPS.
+# SOCIALACCOUNT_AVATAR_SUPPORT
+
+# Dictionary containing provider specific settings.
 SOCIALACCOUNT_PROVIDERS = {
     'orcid': {
         # Base domain of the API. Default value: 'orcid.org', for the production API
@@ -235,6 +291,22 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+ACCOUNT_EMAIL_CONFIRMATION_HMAC = True
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 86400 # 1 day. To strength admin login from being brute forced.
+ACCOUNT_LOGOUT_REDIRECT_URL ='/accounts/login/' # where the user lands after logout
+LOGIN_REDIRECT_URL = '/' # redirects to profile page by default
+ACCOUNT_PRESERVE_USERNAME_CASING = False # reduces the delays in iexact lookups
+ACCOUNT_USERNAME_VALIDATORS = None
+
+
+#Account Signup
+ACCOUNT_FORMS = {'signup': '.forms.SignupForm',}
+
+#Social Account Settings - Provider specific settings
+
 SOCIALACCOUNT_QUERY_EMAIL=ACCOUNT_EMAIL_REQUIRED
 SOCIALACCOUNT_EMAIL_REQUIRED=ACCOUNT_EMAIL_REQUIRED
 SOCIALACCOUNT_STORE_TOKENS=False
