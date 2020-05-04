250341641bf4c50e5ed4769a1e560985a623b6e6
christopherkullenberg <christopher.kullenberg@gmail.com>
2020-05-04 11:57:13 +0200

    added translations and updated model for article to include source, volume, issue

--- ArcsSystem/publications/models.py
+++ ArcsSystem/publications/models.py
@@ -27,6 +27,9 @@ class Article(Publication):
     doi = models.CharField(max_length=200, default="doi")
     py = models.IntegerField(default="0")
     authors = models.CharField(max_length=500, default="Author")
+    source = models.CharField(max_length=200, default="Journal Name")
+    volume = models.CharField(max_length=200, default="Volume")
+    issue = models.CharField(max_length=200, default="Issue")
 
 
     def __str__(self):
@@ -36,5 +39,6 @@ class Article(Publication):
 class Arcsreport(Publication):
     '''
     '''
+
     def __str__(self):
         return self.title
