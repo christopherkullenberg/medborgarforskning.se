d9d679860566147e14dd180d68af36f6ebc8ff24
christopherkullenberg <christopher.kullenberg@gmail.com>
2020-05-11 21:24:13 +0200

    fixed templatetags for all categories ordered and category by query

--- ArcsSystem/staticpages/views.py
+++ ArcsSystem/staticpages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from staticpages.models import Page
+from django.db.models import Q
 
 class StaticDetailView(DetailView):
     model = Page
@@ -11,3 +12,21 @@ class StaticDetailView(DetailView):
 class PageListView(ListView):
     model = Page
     template_name = 'staticpages/staticpage_list.html'
+
+
+
+class TreeView(ListView):
+    model = Page
+
+    def get_categories():
+        object_list = Page.objects.all().order_by('category')
+        return object_list
+
+
+class CategoryView(ListView):
+    model = Page
+
+    def get_category(query):
+        object_list = Page.objects.filter(
+            Q(category__icontains=query)).distinct()
+        return object_list
