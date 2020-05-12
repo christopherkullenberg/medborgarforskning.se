c53018b9a1102fde5b3ad06ddb96af0ab7f28bdf
christopherkullenberg <christopher.kullenberg@gmail.com>
2020-05-12 20:02:31 +0200

    more work on the workpackage app

--- ArcsSystem/workpackages/models.py
+++ ArcsSystem/workpackages/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 from taggit.managers import TaggableManager
 
-# Create your models here.
-
-class Theme(models.Model):
-    title = models.TextField()
-    body = models.TextField()
-    related_papers_tags = TaggableManager()
-
-    def __str__(self):
-        return f'{self.title}'
-
 
 class WorkPackage(models.Model):
     name = models.TextField()
     introduction = models.TextField()
     detailed_content = models.TextField()
-    themes = models.ManyToManyField(Theme)
-    
+
     def __str__(self):
         return self.title
+
+
+class Theme(models.Model):
+    title = models.TextField()
+    body = models.TextField()
+    related_papers_tags = TaggableManager()
+    wp_parent = models.ForeignKey(WorkPackage,
+                                  default=1,
+                                  verbose_name="Work Package",
+                                  on_delete=models.SET_DEFAULT))
+
+    def __str__(self):
+        return f'{self.title}'
