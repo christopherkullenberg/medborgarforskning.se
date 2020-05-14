8f14c854332b1ed686079a1a669be4b8c43cd366
Jonathan Brier <gitcommit@brierjon.com>
2020-05-14 13:14:37 +0200

    functional translation of the title and abstract of articles

--- ArcsSystem/publications/translation.py
+++ ArcsSystem/publications/translation.py
@@ -1,1 +1,16 @@
 from modeltranslation.translator import translator, TranslationOptions
+from .models import Article, Publication
+
+# Supported fields - https://django-modeltranslation.readthedocs.io/en/latest/registration.html#supported-fields-matrix
+
+# One class per model defining which Model and Fields are registerd for translation via django-modeltranslation
+# https://django-modeltranslation.readthedocs.io/en/latest/registration.html#registration
+class PublicationTranslationOptions(TranslationOptions):
+    pass
+class ArticleTranslationOptions(TranslationOptions):
+    ''' Registers blog post fields for translation  '''
+    fields = ('title', 'abstract')
+
+# order matters if you are loading a model depedning on another model
+translator.register(Publication, PublicationTranslationOptions)
+translator.register(Article, ArticleTranslationOptions)
