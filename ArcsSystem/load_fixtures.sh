749b4c6c2415ecc72730d79ca9315b904e4af98d
christopherkullenberg <christopher.kullenberg@gmail.com>
2020-10-15 20:13:15 +0200

    Added basic structure fixtures for staticpages and updated the load_fixtures.sh scrip

--- ArcsSystem/load_fixtures.sh
+++ ArcsSystem/load_fixtures.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 echo Loading fixtures, this may take a moment...
 
+echo Loading workpackages... 
 python manage.py loaddata workpackages/fixtures/workpackages.xml
 echo Workpackages fixtures loaded!
 
+echo Loading publications... this one is big....
 python manage.py loaddata publications/fixtures/publications.xml
 echo Publications fixtures loaded
 
+echo Loading organizations...
 python manage.py loaddata organizations/fixtures/organizations.xml
 echo Organizations fixtures loaded
+
+echo Loading staticpages...
+python manage.py loaddata staticpages/fixtures/staticpages.xml
+echo Staticpages fixtures loaded
+
 
 
 echo Done. Fixtures loaded [unless you saw errors].
