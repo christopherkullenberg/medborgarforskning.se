59747f3f4c3d167b25d4b7b7809c19a127655893
christopherkullenberg <christopher.kullenberg@gmail.com>
2020-11-02 10:29:07 +0100

    Fixed replacement of forwardslash to ampersand in the keywords.

--- ArcsSystem/keywords/views.py
+++ ArcsSystem/keywords/views.py
@@ -25,21 +25,6 @@ def template_view(request):
     return render(request, 'exampletemplate.html', context)
 '''
 
-def bad_code_update_bw_slach():
-
-    print("start")
-
-    for kw in KeywordEng.objects.filter(keyword__contains="/"):
-        kw.keyword = kw.keyword.replace("/", "&")
-        kw.save()
-
-    for kw in KeywordSwe.objects.filter(keyword__contains="/"):
-        kw.keyword = kw.keyword.objects.replace("/", "&")
-        kw.save()
-
-
-
-bad_code_update_bw_slach()
 
 class KeywordList(ListView):
     model = KeywordEng
@@ -206,5 +191,3 @@ def update_object_KWL(obj, request,  stop):
                 obj.keyword_lines.add(options.first())
 
     obj.save()
-
-
