646f5fea728d2861c69f697a0358cf14ef18804e
christopherkullenberg <christopher.kullenberg@gmail.com>
2020-11-06 10:25:45 +0100

    Put db in gitignore

--- ArcsSystem/dump_database.sh
+++ ArcsSystem/dump_database.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "Dumping database to xml file db.xml..."
-python manage.py dumpdata --exclude auth.permission --format xml > db.xml
+python manage.py dumpdata --format xml > db.xml
 echo "Done! [unless you saw errors]"
-
-
