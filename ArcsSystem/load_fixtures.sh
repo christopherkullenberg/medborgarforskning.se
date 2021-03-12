7121746016513fe9166cff9374a292703b381385
christopherkullenberg <christopher.kullenberg@gmail.com>
2021-03-12 19:59:55 +0100

    load_fixtures.sh only load staticpages and workpackages prvnt conflict

--- ArcsSystem/load_fixtures.sh
+++ ArcsSystem/load_fixtures.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 echo Loading fixtures, this may take a moment...
 
-python manage.py loaddata db.xml
+#python manage.py loaddata db.xml
 
 
-: <<'END'
 
-echo 1. Loading organizations...
-python manage.py loaddata organizations/fixtures/organizations.xml
-echo Organizations loaded
 
-echo 2. Loading Projects and keyword lines... this one is big....
-python manage.py loaddata projects/fixtures/projects.xml
-echo Projects and Keyword lines fixtures loaded
+#echo 1. Loading organizations...
+#python manage.py loaddata organizations/fixtures/organizations.xml
+#echo Organizations loaded
 
-echo 3. Loading keywords and publications... this one is big....
-python manage.py loaddata publications/fixtures/publications.xml
-echo Publications fixtures loaded
+#echo 2. Loading Projects and keyword lines... this one is big....
+#python manage.py loaddata projects/fixtures/projects.xml
+#echo Projects and Keyword lines fixtures loaded
+
+#echo 3. Loading keywords and publications... this one is big....
+#python manage.py loaddata publications/fixtures/publications.xml
+#echo Publications fixtures loaded
 
 echo 4. Loading workpackages...
 python manage.py loaddata workpackages/fixtures/workpackages.xml
@@ -26,7 +26,7 @@ echo 5. Loading staticpages...
 python manage.py loaddata staticpages/fixtures/staticpages.xml
 echo Staticpages fixtures loaded
 
-END
+
 
 
 echo Done. Fixtures loaded [unless you saw errors].
